fe419c0d295c02f00961140883a68e834dcf1bb5
Jose Manuel Abuin Mosquera <jabuinmo@iaimz075.Informatik.Uni-Mainz.DE>
2017-02-02 11:12:42 +0100

    Uncommented nativeutils

--- src/main/java/com/github/metacachespark/HashFunctions.java
+++ src/main/java/com/github/metacachespark/HashFunctions.java
@@ -1,6 +1,6 @@
 package com.github.metacachespark;
 
-//import cz.adamh.utils.NativeUtils;
+import cz.adamh.utils.NativeUtils;
 
 import java.io.IOException;
 import java.io.Serializable;
@@ -9,7 +9,7 @@ import java.io.Serializable;
  * Created by chema on 1/16/17.
  */
 public class HashFunctions implements Serializable {
-/*
+
         static {
             try {
                 NativeUtils.loadLibraryFromJar("/libmetacache.so");
@@ -17,7 +17,7 @@ public class HashFunctions implements Serializable {
                 e.printStackTrace();
             }
         }
-*/
+
 	public static int thomas_mueller_hash(int x) {
 		return thomas_mueller_hash32(x);
 	}
