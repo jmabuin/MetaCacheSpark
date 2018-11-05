96290b38d425fa916df0fe3510a9f61eae3875f0
Jose M. Abuin <chemaster@gmail.com>
2018-11-05 17:44:02 +0100

    Scripts modified according new input options

--- script/QueryHiSeq.sh
+++ script/QueryHiSeq.sh
@@ -5,6 +5,7 @@ PARTITION_NUMBER=32
 OUTPUT_DIR=Genomica/Output_HiSeq_Native_$PARTITION_NUMBER
 DATABASE_NAME=DatabaseNative_$PARTITION_NUMBER
 BUFFER_SIZE=150000
-echo "spark-submit --class com.github.jmabuin.metacachespark.MetaCacheSpark --num-executors $PARTITION_NUMBER --master yarn --executor-memory 15G --driver-memory 12G MetaCacheSpark-0.3.0.jar -m query -d hashmultimap_native -p $PARTITION_NUMBER -b $BUFFER_SIZE $DATABASE_NAME $OUTPUT_DIR $INPUT_SEQUENCES"
-spark-submit --class com.github.jmabuin.metacachespark.MetaCacheSpark --num-executors $PARTITION_NUMBER --master yarn --executor-memory 15G --driver-memory 12G MetaCacheSpark-0.3.0.jar -m query -d hashmultimap_native -p $PARTITION_NUMBER -b $BUFFER_SIZE $DATABASE_NAME $OUTPUT_DIR $INPUT_SEQUENCES
+RESULTS_SIZE=32
+echo "spark-submit --class com.github.jmabuin.metacachespark.MetaCacheSpark --num-executors $PARTITION_NUMBER --master yarn --executor-memory 15G --driver-memory 12G MetaCacheSpark-0.3.0.jar -m query -d hashmultimap_native -p $PARTITION_NUMBER -b $BUFFER_SIZE -s $RESULTS_SIZE $DATABASE_NAME $OUTPUT_DIR $INPUT_SEQUENCES"
+spark-submit --class com.github.jmabuin.metacachespark.MetaCacheSpark --num-executors $PARTITION_NUMBER --master yarn --executor-memory 15G --driver-memory 12G MetaCacheSpark-0.3.0.jar -m query -d hashmultimap_native -p $PARTITION_NUMBER -b $BUFFER_SIZE -s $RESULTS_SIZE $DATABASE_NAME $OUTPUT_DIR $INPUT_SEQUENCES
 #--conf spark.yarn.driver.memoryOverhead=1500
