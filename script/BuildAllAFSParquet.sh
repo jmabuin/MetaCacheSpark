bcc4cccec274fa6b465f975a4ef982c3e41c365a
Jose M. Abuin <chemaster@gmail.com>
2018-12-17 13:28:50 +0100

    Added new scripts for database creation

---
+++ script/BuildAllAFSParquet.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+INPUT_SEQUENCES=Genomica/InputDatasets/AFS/RefSeq
+TAXONOMY=Genomica/InputDatasets/RefSeq90/Refseq-Release90-Taxonomy
+PARTITION_NUMBER=32
+DATABASE_NAME=DatabaseNativeAFS_Parquet_$PARTITION_NUMBER
+
+spark-submit --class com.github.jmabuin.metacachespark.MetaCacheSpark --num-executors $PARTITION_NUMBER --master yarn --executor-memory 25G --driver-memory 20G MetaCacheSpark-0.3.0.jar -m build -d parquet -w -p $PARTITION_NUMBER -t $TAXONOMY $DATABASE_NAME $INPUT_SEQUENCES
