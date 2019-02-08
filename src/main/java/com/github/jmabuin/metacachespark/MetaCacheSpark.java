aa9a92fe4dba0f745539dc1f45a3bce1670849a1
Jose M. Abuin <chemaster@gmail.com>
2019-02-08 08:39:24 +0100

    Changed intentation

--- src/main/java/com/github/jmabuin/metacachespark/MetaCacheSpark.java
+++ src/main/java/com/github/jmabuin/metacachespark/MetaCacheSpark.java
@@ -34,75 +34,75 @@ import java.util.TreeMap;
  */
 public class MetaCacheSpark implements Serializable {
 
-	private static final Log LOG = LogFactory.getLog(MetaCacheSpark.class);
+    private static final Log LOG = LogFactory.getLog(MetaCacheSpark.class);
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		long initTime = System.nanoTime();
+        long initTime = System.nanoTime();
 
-		MetaCacheOptions newOptions = new MetaCacheOptions(args);
+        MetaCacheOptions newOptions = new MetaCacheOptions(args);
 
-		if(newOptions.getMode() == EnumModes.Mode.HELP) {
-			newOptions.printHelp();
-		}
-		else if(newOptions.getMode() == EnumModes.Mode.BUILD) {
-			// Build mode entry point
+        if(newOptions.getMode() == EnumModes.Mode.HELP) {
+            newOptions.printHelp();
+        }
+        else if(newOptions.getMode() == EnumModes.Mode.BUILD) {
+            // Build mode entry point
 
-			SparkConf sparkConf = new SparkConf().setAppName("MetaCacheSpark - Build");
+            SparkConf sparkConf = new SparkConf().setAppName("MetaCacheSpark - Build");
 
-			//sparkConf.set("spark.sql.parquet.mergeSchema", "false");
-			sparkConf.set("spark.shuffle.reduceLocality.enabled","false");
-			//sparkConf.set("spark.memory.useLegacyMode","true");
-			//sparkConf.set("spark.storage.memoryFraction", "0.2");
+            //sparkConf.set("spark.sql.parquet.mergeSchema", "false");
+            sparkConf.set("spark.shuffle.reduceLocality.enabled","false");
+            //sparkConf.set("spark.memory.useLegacyMode","true");
+            //sparkConf.set("spark.storage.memoryFraction", "0.2");
 
-			sparkConf.set("spark.sql.tungsten.enabled", "true");
-			sparkConf.set("spark.io.compression.codec", "snappy");
-			sparkConf.set("spark.sql.parquet.compression.codec", "snappy");
+            sparkConf.set("spark.sql.tungsten.enabled", "true");
+            sparkConf.set("spark.io.compression.codec", "snappy");
+            sparkConf.set("spark.sql.parquet.compression.codec", "snappy");
 
-			// Kryo serializer
-			sparkConf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer");
-			sparkConf.set("spark.kryoserializer.buffer.max","1024m");
+            // Kryo serializer
+            sparkConf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer");
+            sparkConf.set("spark.kryoserializer.buffer.max","1024m");
 
-			Class[] serializedClasses = {Location.class, Sketch.class, TreeMap.class, LocationBasic.class, MatchCandidate.class, List.class};
-			sparkConf.registerKryoClasses(serializedClasses);
+            Class[] serializedClasses = {Location.class, Sketch.class, TreeMap.class, LocationBasic.class, MatchCandidate.class, List.class};
+            sparkConf.registerKryoClasses(serializedClasses);
 
 
-			//The ctx is created from the previous config
-			JavaSparkContext ctx = new JavaSparkContext(sparkConf);
-			//ctx.hadoopConfiguration().set("parquet.enable.summary-metadata", "false");
+            //The ctx is created from the previous config
+            JavaSparkContext ctx = new JavaSparkContext(sparkConf);
+            //ctx.hadoopConfiguration().set("parquet.enable.summary-metadata", "false");
 
-			LOG.info("Using Spark :: " + ctx.version());
+            LOG.info("Using Spark :: " + ctx.version());
 
-			Build buildObject = new Build(newOptions, ctx);
+            Build buildObject = new Build(newOptions, ctx);
 
-			buildObject.buildDatabase();
+            buildObject.buildDatabase();
 
             long endTime = System.nanoTime();
 
             LOG.info("End of program. Total time: " + ((endTime - initTime) / 1e9) + " seconds");
 
-			///ctx.close();
-		}
-		else if(newOptions.getMode() == EnumModes.Mode.QUERY) {
-			SparkConf sparkConf = new SparkConf().setAppName("MetaCacheSpark - Query");
+            ///ctx.close();
+        }
+        else if(newOptions.getMode() == EnumModes.Mode.QUERY) {
+            SparkConf sparkConf = new SparkConf().setAppName("MetaCacheSpark - Query");
 
-			//sparkConf.set("spark.sql.parquet.mergeSchema", "false");
-			sparkConf.set("spark.shuffle.reduceLocality.enabled","false");
-			//sparkConf.set("spark.memory.useLegacyMode","true");
-			//sparkConf.set("spark.storage.memoryFraction", "0.3");
+            //sparkConf.set("spark.sql.parquet.mergeSchema", "false");
+            sparkConf.set("spark.shuffle.reduceLocality.enabled","false");
+            //sparkConf.set("spark.memory.useLegacyMode","true");
+            //sparkConf.set("spark.storage.memoryFraction", "0.3");
 
-			// Kryo serializer
-			sparkConf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer");
-			sparkConf.set("spark.kryoserializer.buffer.max","1024m");
-			sparkConf.set("spark.driver.maxResultSize", "4g");
+            // Kryo serializer
+            sparkConf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer");
+            sparkConf.set("spark.kryoserializer.buffer.max","1024m");
+            sparkConf.set("spark.driver.maxResultSize", "4g");
 
-			sparkConf.set("spark.sql.tungsten.enabled", "true");
-			sparkConf.set("spark.io.compression.codec", "snappy");
-			sparkConf.set("spark.sql.parquet.compression.codec", "snappy");
+            sparkConf.set("spark.sql.tungsten.enabled", "true");
+            sparkConf.set("spark.io.compression.codec", "snappy");
+            sparkConf.set("spark.sql.parquet.compression.codec", "snappy");
 
-			if (newOptions.getNumThreads() != 1) {
-				sparkConf.set("spark.executor.cores", String.valueOf(newOptions.getNumThreads()));
-			}
+            if (newOptions.getNumThreads() != 1) {
+                sparkConf.set("spark.executor.cores", String.valueOf(newOptions.getNumThreads()));
+            }
 
 /*
 			TreeMap<LocationBasic, Integer> proba = new TreeMap<LocationBasic, Integer>(new LocationBasicComparator());
@@ -119,33 +119,33 @@ public class MetaCacheSpark implements Serializable {
 
 			MapSerializer serializer = new MapSerializer();
 */
-			//sparkConf.set("spark.kryo.registrator","com.github.jmabuin.metacachespark.MyKryoRegistrator");
-			Class[] serializedClasses = {Location.class, Sketch.class, TreeMap.class, LocationBasic.class, MatchCandidate.class, List.class};
-			sparkConf.registerKryoClasses(serializedClasses);
+            //sparkConf.set("spark.kryo.registrator","com.github.jmabuin.metacachespark.MyKryoRegistrator");
+            Class[] serializedClasses = {Location.class, Sketch.class, TreeMap.class, LocationBasic.class, MatchCandidate.class, List.class};
+            sparkConf.registerKryoClasses(serializedClasses);
 
 
-			//The ctx is created from the previous config
-			JavaSparkContext ctx = new JavaSparkContext(sparkConf);
-			//ctx.hadoopConfiguration().set("parquet.enable.summary-metadata", "false");
+            //The ctx is created from the previous config
+            JavaSparkContext ctx = new JavaSparkContext(sparkConf);
+            //ctx.hadoopConfiguration().set("parquet.enable.summary-metadata", "false");
             ctx.setLogLevel("WARN");
-			LOG.warn("Using Spark version - " + ctx.version());
+            LOG.warn("Using Spark version - " + ctx.version());
 
-			// Get arguments and do my stuff
-			//String queryArgs[] = newOptions.getOtherOptions();
-			Query queryObject = new Query(newOptions, ctx);
+            // Get arguments and do my stuff
+            //String queryArgs[] = newOptions.getOtherOptions();
+            Query queryObject = new Query(newOptions, ctx);
 
 
             long endTime = System.nanoTime();
 
             LOG.warn("End of program. Total time: " + ((endTime - initTime) / 1e9) + " seconds");
-		}
-		else {
-			System.out.println("Not recognized option");
-			newOptions.printHelp();
-			//System.exit(1);
-		}
+        }
+        else {
+            System.out.println("Not recognized option");
+            newOptions.printHelp();
+            //System.exit(1);
+        }
 
 
-		//System.exit(0);
-	}
+        //System.exit(0);
+    }
 }
