64d9068a75a114ff6fa50b8368e996db07520ec7
Jose M. Abuin <chemaster@gmail.com>
2019-02-08 08:38:19 +0100

    Added parameter to clone metacache build behaviour

--- script/BuildAllAFSBig.sh
+++ script/BuildAllAFSBig.sh
@@ -5,4 +5,4 @@ TAXONOMY=Genomica/InputDatasets/RefSeq90/Refseq-Release90-Taxonomy
 PARTITION_NUMBER=64
 DATABASE_NAME=DatabaseNativeAFSBig_$PARTITION_NUMBER
 
-spark-submit --class com.github.jmabuin.metacachespark.MetaCacheSpark --num-executors $PARTITION_NUMBER --master yarn --executor-memory 25G --driver-memory 12G MetaCacheSpark-0.3.0.jar -m build -d hashmultimap_native -w -p $PARTITION_NUMBER -t $TAXONOMY $DATABASE_NAME $INPUT_SEQUENCES
+spark-submit --class com.github.jmabuin.metacachespark.MetaCacheSpark --num-executors $PARTITION_NUMBER --master yarn --executor-memory 25G --driver-memory 12G MetaCacheSpark-0.3.0.jar -m build -d hashmultimap_native -w -l -p $PARTITION_NUMBER -t $TAXONOMY $DATABASE_NAME $INPUT_SEQUENCES
