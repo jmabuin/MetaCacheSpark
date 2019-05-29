8e86d36db732c0fc7af6a0c3e434ad11c7af073f
Jose M. Abuin <chemaster@gmail.com>
2019-05-29 17:11:12 +0200

    Modified options in scripts according to the new ones

--- script/BuildAllAFS20-32.sh
+++ script/BuildAllAFS20-32.sh
@@ -6,4 +6,4 @@ PARTITION_NUMBER=32
 DATABASE_NAME=DatabaseNativeAFS20_$PARTITION_NUMBER
 
 #spark-submit --class com.github.jmabuin.metacachespark.MetaCacheSpark --num-executors $PARTITION_NUMBER --master yarn --executor-memory 30G --driver-memory 12G MetaCacheSpark-0.3.0.jar -m build -d hashmultimap_native -w -p $PARTITION_NUMBER -t $TAXONOMY $DATABASE_NAME $INPUT_SEQUENCES
-spark-submit --class com.github.jmabuin.metacachespark.MetaCacheSpark --conf spark.network.timeout=10000000  --num-executors $PARTITION_NUMBER --master yarn --executor-memory 60G --driver-memory 20G MetaCacheSpark-0.3.0.jar -m build -d hashmultimap_native -w -o -p $PARTITION_NUMBER -t $TAXONOMY $DATABASE_NAME $INPUT_SEQUENCES
+spark-submit --class com.github.jmabuin.metacachespark.MetaCacheSpark --conf spark.network.timeout=10000000  --num-executors $PARTITION_NUMBER --master yarn --executor-memory 60G --driver-memory 20G MetaCacheSpark-0.3.0.jar -m build -d hashmultimap_native -o -p $PARTITION_NUMBER -t $TAXONOMY $DATABASE_NAME $INPUT_SEQUENCES
--- script/BuildAllAFS31RS90-128.sh
+++ script/BuildAllAFS31RS90-128.sh
@@ -5,4 +5,4 @@ TAXONOMY=Genomica/InputDatasets/Taxonomy
 PARTITION_NUMBER=128
 DATABASE_NAME=DatabaseNativeAFS31RS90_$PARTITION_NUMBER
 
-spark-submit --class com.github.jmabuin.metacachespark.MetaCacheSpark --conf spark.network.timeout=10000000 --num-executors $PARTITION_NUMBER --master yarn --executor-memory 24G --driver-memory 20G MetaCacheSpark-0.3.0.jar -m build -d hashmultimap_native -w -o -p $PARTITION_NUMBER -t $TAXONOMY $DATABASE_NAME $INPUT_SEQUENCES
+spark-submit --class com.github.jmabuin.metacachespark.MetaCacheSpark --conf spark.network.timeout=10000000 --num-executors $PARTITION_NUMBER --master yarn --executor-memory 24G --driver-memory 20G MetaCacheSpark-0.3.0.jar -m build -d hashmultimap_native -o -p $PARTITION_NUMBER -t $TAXONOMY $DATABASE_NAME $INPUT_SEQUENCES
